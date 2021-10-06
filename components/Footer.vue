ded9bcc13f84c37b1ee6f60435e4cbca703e2522
Guillim <guillim@users.noreply.github.com>
2021-10-06 18:38:45 +0200

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <footer class="mt-12 bg-gray-800">
     <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8">
-      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="grid grid-cols-2 gap-8 xl:col-span-2">
-          <div class="md:grid md:grid-cols-2 md:gap-8">
+      <div class="">
+        <div class="">
+          <div class="">
             <div>
-              <ul class="mt-4 space-y-4">
+              <ul class="mt-4 space-y-4 flex justify-between">
                 <li>
                   <nuxt-link
                     to="/"
